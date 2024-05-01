c01d2dd968d299dd09b17b19ac8933381556d0fc
NCaur <navdeepk27892@gmail.com>
2024-05-01 16:31:39 -0400

    Changes

---
+++ Vid057_Super_Global_Variable.php
@@ -0,0 +1,12 @@
+<?php
+$a = 10;
+print_r($GLOBALS);// displays the details of all the user-defined global variables within the project; Have to include the files
+//print_r($_SERVER); displays server related details
+//print_r($_Request); Refer file Vid56
+//print_r($_get); Refer Video54
+//print_r($_post); Refer Video55
+//print_r($_Files); Display all the uploaded files(eg; photos,pdf)
+//print_r($_Cookies); Display the temporary data for website visited for 1st time
+//print_r($_Session); data remain maintained
+//print_r($_Env); details of the environment being used
+?>
